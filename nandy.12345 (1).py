0d6a86fd76347a5a7f8ce599c02899b49af378d1
NukanaboyeNandini9030 <162982869+NukanaboyeNandini9030@users.noreply.github.com>
2024-03-11 21:15:54 +0530

    Add files via upload

---
+++ nandy.12345 (1).py
@@ -0,0 +1,14 @@
+flavours of python
+c python-> Written in c, most common imple,entation of python
+Jython-> java implementation of the python progtamming which is tightly Integreated with. net/
+brython Browsa python ,run in browsers
+kuby python Bridge b\w python&ruby tnter preters
+micropython runs in micro controller
+what kind pf language is a python
+python is a
+object oriented programming language
+dynamically typed language
+scripted language
+interpreted language
+compiled language
+functional programming language
