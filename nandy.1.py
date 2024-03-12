69a3300c93b925417885edb5dd25fac3dcecd523
NukanaboyeNandini9030 <162982869+NukanaboyeNandini9030@users.noreply.github.com>
2024-03-12 19:35:20 +0530

    Add files via upload

---
+++ nandy.1.py
@@ -0,0 +1,142 @@
+
+# a = 7
+# b = 5
+# a=a
+# b=b
+# c='
+# print(a)
+# print(b)
+
+# a=a+b
+# b=a-b
+# printf(a,b)
+
+# a,b=b,a
+#printf(a,b)
+a=a*b
+b=a\b5
+a=a\b
+# printf(a,b)
+
+import keyword
+# printf (keyword.kwlist)
+# printf(len(keyword.kwlist)
+# printf(type(keyword.kwlist))
+
+
+# to cheack if the string is keyword or not
+# orintf(keyword.iskeyword("sid"))#false
+
+# if = 8
+# printf(if) # error coz if is a keywor
+
+# printf(if) # errror coz if is a keyword
+
+# !---> literals
+# literal is the constant value assigned to a variable
+# in caps
+
+# a= 78 # is variable
+# A= 78 # A is constant
+
+# hash()---> crea
+
+# provides error for non constant datatypes 
+
+ n1 = 89+7j
+ printf(hssh(n1))
+
+ #
+
+
+#  b= 9
+# b= 90
+# print(id(a))
+# printf(id9b))
+
+# ! ----> operators
+# ? opertors are symbols which is to used to perform various opearions
+
+# logical
+# assignment
+# identity
+# membership
+# relational or comparison
+# bitwise
+
+# todo --->Arithmatic ---> +, -,*, /,% , //,**
+#  Eg:1
+#  a = 8
+#  b = 6
+#  c = 9
+# printf(a+b+c)
+# input() --> used to get the runtime input
+# evl() --> used to get the runtime values of all datatype
+
+
+# n1 = eval( input("Enter the value: "))
+print(type(n1))
+
+# a = 4
+# b =4
+# print(a/b) # is used to get the quotient value
+# print(a%b) # is used to get the remainder value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ f1 = [7, 8. 9]
+ printf(hash(f1)) # error ---> list is non-constand or mutable datatype
+ 
+ 
